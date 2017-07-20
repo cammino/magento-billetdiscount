6198305fbce21352a1aa3bfdf5ac1042caee4924
Marcio Junior <marciofmjr@hotmail.com>
2017-07-20 11:15:46 -0300

    Cria estrutura básica do módulo

---
+++ Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Data
+ *
+ * This file is contains some functions to help other methods in module
+ *
+ * @category   Cammino
+ * @package    Billetdiscount
+ * @author     Cammino Digital <contato@cammino.com.br>
+ */
+
+class Cammino_Billetdiscount_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	
+}
